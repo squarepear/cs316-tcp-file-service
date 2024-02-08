d5945d1f41941de06c0389655a99a235eb3db4d8
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2024-02-08 13:47:42 -0500

    Add inital client communication

--- src/cx/pear/tcpfileservice/Client.java
+++ src/cx/pear/tcpfileservice/Client.java
@@ -1,15 +1,29 @@
 package cx.pear.tcpfileservice;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         if (args.length != 2) {
-            System.out.println("WHAT DO YOU THINK YOU ARE DOING?");
+            System.out.println("Syntax: Client <ServerIP> <ServerPort>");
             return;
         }
 
+        InetAddress serverIP = InetAddress.getByName(args[0]);
         int serverPort = Integer.parseInt(args[1]);
+        InetSocketAddress serverAddress = new InetSocketAddress(serverIP, serverPort);
+
+        if (serverPort <= 0) {
+            System.out.println("ServerPort must be a positive integer!");
+            return;
+        }
 
         String command = "";
 
@@ -21,19 +35,19 @@ public class Client {
 
             switch (command) {
                 case "upload":
-                    uploadFile();
+                    uploadFile(serverAddress);
                     continue;
                 case "download":
-                    downloadFile();
+                    downloadFile(serverAddress);
                     continue;
                 case "rename":
-                    renameFile();
+                    renameFile(serverAddress);
                     continue;
                 case "delete":
-                    deleteFile();
+                    deleteFile(serverAddress);
                     continue;
                 case "list":
-                    listFiles();
+                    listFiles(serverAddress);
                     continue;
                 case "quit":
                     continue;
@@ -47,30 +61,57 @@ public class Client {
 
     private static void printHelp() {
         System.out.println("Upload");
+        System.out.println("Download");
+        System.out.println("Rename");
+        System.out.println("Delete");
+        System.out.println("List");
         System.out.println("Quit");
-        System.out.println("Download");
-        System.out.println("Rename");
-        System.out.println("Delete");
-        System.out.println("List");
     }
 
-    private static void uploadFile() {
-        System.out.println("Upload");
+    private static void uploadFile(InetSocketAddress address) {
+        System.out.println(Arrays.toString(sendRequest(address, ByteBuffer.wrap("C".getBytes()))));
     }
 
-    private static void downloadFile() {
-        System.out.println("Download");
+    private static void downloadFile(InetSocketAddress address) {
+        System.out.println(Arrays.toString(sendRequest(address, ByteBuffer.wrap("R".getBytes()))));
     }
 
-    private static void renameFile() {
-        System.out.println("Rename");
+    private static void renameFile(InetSocketAddress address) {
+        System.out.println(Arrays.toString(sendRequest(address, ByteBuffer.wrap("U".getBytes()))));
     }
 
-    private static void deleteFile() {
-        System.out.println("Delete");
+    private static void deleteFile(InetSocketAddress address) {
+        System.out.println(Arrays.toString(sendRequest(address, ByteBuffer.wrap("D".getBytes()))));
     }
 
-    private static void listFiles() {
-        System.out.println("List");
+    private static void listFiles(InetSocketAddress address) {
+        System.out.println(Arrays.toString(sendRequest(address, ByteBuffer.wrap("L".getBytes()))));
+    }
+
+    private static byte[] sendRequest(InetSocketAddress address, ByteBuffer message) {
+        try (SocketChannel channel = SocketChannel.open()) {
+            System.out.println("AAA");
+
+            channel.connect(address);
+            channel.write(message);
+
+            System.out.println("BBB");
+
+            ByteBuffer replyBuffer = ByteBuffer.allocate(1024);
+            int bytesRead = channel.read(replyBuffer);
+
+            System.out.println("CCC");
+
+            replyBuffer.flip();
+
+            byte[] replyArray = new byte[bytesRead];
+            replyBuffer.get(replyArray);
+
+            System.out.println("DDD");
+
+            return replyArray;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
